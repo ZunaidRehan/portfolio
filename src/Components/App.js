877e8e2694a2e2eca45a118e97323d3a2483e63f
Zunaid Alam <zunaidrehan786@gmail.com>
2023-03-31 17:03:36 +0530

    Insta link changed

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 
 import './../App.scss';
 import { Banner } from './Banner/Banner';
+import { Professional } from './Professional/Professional';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Professional />
     </div>
   );
 }
---
+++ src/Components/Professional/Professional.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './professional.scss'
+export const Professional = () => {
+    return (
+        <div className='container'>
+            Professional
+        </div>
+    )
+}
